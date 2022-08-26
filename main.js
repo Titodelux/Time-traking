0742d50021291e83a41b75d410aee2fc63c2a9f7
Ángel Pérez <angelperez.r007@gmail.com>
2022-08-26 14:09:12 -0400

    Version oficial :D

--- main.js
+++ main.js
@@ -1,37 +1,45 @@
-// const dailyButton = document.querySelector("#dailyButton")
-// const weeklyButton = document.querySelector("#weeklyButton")
-// const monthlyButton = document.querySelector("#monthlyButton")
 
-// const button = document.querySelectorAll(".button")
+const button = document.querySelectorAll(".button")
+
 const dailyData = document.querySelector(".dailyData")
 const weeklyData = document.querySelector(".weeklyData")
 const monthlyData = document.querySelector(".monthlyData")
 
 
-function translateDay() {
 
-    // if (!(button.classList.contains("activate"))){
-    //     console.log(button.classList)
-    // }
-    
+function translateDay() {
+    button[0].classList.add("activate")   
+    button[1].classList.remove("activate")   
+    button[2].classList.remove("activate")   
+
     dailyData.style.transform = "translateX(0%)"
     weeklyData.style.transform = "translateX(100%)"
     monthlyData.style.transform = "translateX(200%)"
     
 }
 function translateWeek() {
-    // data.style.backgroundColor = "red"
+    button[1].classList.add("activate")   
+    button[0].classList.remove("activate")   
+    button[2].classList.remove("activate")   
+
     dailyData.style.transform = "translateX(-100%)"
     weeklyData.style.transform = "translateX(0%)"
     monthlyData.style.transform = "translateX(100%)"
-
     
 }
 function translateMonth() {
-    // data.style.backgroundColor = "red"
+    button[2].classList.add("activate")   
+    button[0].classList.remove("activate")   
+    button[1].classList.remove("activate")   
+
     dailyData.style.transform = "translateX(-200%)"
     weeklyData.style.transform = "translateX(-100%)"
     monthlyData.style.transform = "translateX(0%)"
 
 }
 
+function changeColor(){
+
+}
+
+dailyButton.addEventListener("active", translateDay())
